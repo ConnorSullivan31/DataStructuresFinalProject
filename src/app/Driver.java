9475139674cf6fdc2a6bc8497bac22d4e03209f6
ConnorSullivan31 <34195787+ConnorSullivan31@users.noreply.github.com>
2022-04-27 15:31:38 -0500

    Removed print statement

--- src/app/Driver.java
+++ src/app/Driver.java
@@ -15,7 +15,6 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test 123");
-		System.out.println("Test 2");
 		AppAssembler app = new AppAssembler();//Create our app
 		app.RunWindow();//Run our app
 		
