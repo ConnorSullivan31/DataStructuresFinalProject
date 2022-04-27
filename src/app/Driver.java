e72d16f73235840183e000cb252d3c7e70e88fd5
ConnorSullivan31 <34195787+ConnorSullivan31@users.noreply.github.com>
2022-04-27 15:28:42 -0500

    Added a print statement

--- src/app/Driver.java
+++ src/app/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test 123");
-		
+		System.out.println("Test 2");
 		AppAssembler app = new AppAssembler();//Create our app
 		app.RunWindow();//Run our app
 		
